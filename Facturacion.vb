3fd2258eda97e48087c30d26ca6fcd8fb455bf5e
ecacerest <ecacerest@finagil.com.mx>
2018-04-20 12:19:44 -0500

    Cambio de Concepto con iva factiraje

--- Facturacion.vb
+++ Facturacion.vb
@@ -1130,7 +1130,7 @@ Module GneraFactura
                                     Datos(11) = Math.Round(Math.Round(CDec(Datos(10)), 2) * TasaIVA, 2)
                                 End If
 
-                                If InStr(Datos(8), "GASTOS NOTARIALES") And ROWheader._27_Serie_Comprobante = "F" Then
+                                If InStr(Datos(8), "GASTOS DE RATIFICACION") And ROWheader._27_Serie_Comprobante = "F" Then
                                     ReDim Preserve Datos(11)
                                     Datos(11) = Math.Round(Math.Round(CDec(Datos(10)), 2) * TasaIVA, 2)
                                 End If
